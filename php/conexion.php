c9c00527d5d3edb33f899cabdd4a90f4f2b17b85
matiasiaquinta <matias.iaquinta@gmail.com>
2024-08-18 14:30:54 -0300

    update conexion.php

--- php/conexion.php
+++ php/conexion.php
@@ -2,12 +2,11 @@
 //esto es para los usuarios // me da error
 //session_start();
 
-//Connecting to DB
-$servidor = 'localhost';
-$usuario = 'root';
-$pass = '';
-//$baseDatos = 'test';
-$baseDatos = 'pixie';
+//Connecting to DB - from CleverCloud
+$servidor = 'blu3ym2e15d4ekugiosj-mysql.services.clever-cloud.com';
+$usuario = 'usz5mc90filjqquk';
+$pass = 't0eVzL97rGp5OHV0qSTd';
+$baseDatos = 'blu3ym2e15d4ekugiosj';
 
 //Create Connection
 $conn = new mysqli($servidor, $usuario, $pass, $baseDatos);
